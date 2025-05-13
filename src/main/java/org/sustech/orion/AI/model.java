7083352f0b217ca88de5107a1e75cff949243006
VictorJiang <2380952232@qq.com>
2025-05-13 13:22:12 +0800

    111

--- src/main/java/org/sustech/orion/AI/model.java
+++ src/main/java/org/sustech/orion/AI/model.java
@@ -5,13 +5,20 @@ import com.baidubce.qianfan.QianfanV2;
 import com.baidubce.qianfan.model.chat.v2.request.RequestV2;
 import com.baidubce.qianfan.model.chat.v2.response.ResponseV2;
 
+import java.util.List;
+
 public class model {
     private static final QianfanV2 client = new Qianfan("bce-v3/ALTAK-rN1GJbJxabN8c2V0twWb5/acf494b1376487f599c2aa78c80f1fe8c06462f9").v2();
+    private static final List<String> availableModels = List.of("qwq-32b", "qwen3-32b", "deepseek-r1-distill-qwen-32b", "qwen3-235b-a22b");
+    private static final String defaultModel = "qwq-32b";
 
-    public static String chat(String input){
+    public static String chat(String input, String model) {
         String System = "你是一个智能的AI助手，你可以回答任何问题";
+        if (!availableModels.contains(model)) {
+            model = defaultModel;
+        }
         RequestV2 request = client.chatCompletion()
-                .model("qwq-32b")
+                .model(model)
                 .addMessage("system" , System)
                 .addMessage("user", input)
                 .temperature(0.7)
@@ -19,4 +26,9 @@ public class model {
         ResponseV2 response = client.chatCompletion(request);
         return response.getChoices().get(0).getMessage().getContent();
     }
+
+    public static String llm_plagiarism_check(String subbmissionA, String submissionB, String model) {
+        return "hi";
+    }
 }
+
--- src/main/java/org/sustech/orion/AI/test.java
+++ src/main/java/org/sustech/orion/AI/test.java
@@ -3,18 +3,19 @@ package org.sustech.orion.AI;
 public class test {
     public static void main(String[] args) throws Exception {
         String input =  "你好，你是谁";
-        System.out.println(model.chat(input));
+        //available models: qwq-32b qwen3-32b deepseek-r1-distill-qwen-32b qwen3-235b-a22b
+        System.out.println(model.chat(input, "qwen3-235b-a22b"));
 
-        String text = FileReader.readPdf("D:\\desktop\\江澈.pdf");
-        System.out.println(text);
-
-        text = FileReader.readRawMd("D:\\desktop\\report1\\report1\\report1 - team 5 - zh.md");
-        System.out.println(text);
-
-        text = FileReader.readDocx("D:\\desktop\\简历.docx");
-        System.out.println(text);
-
-        String photo = "D:\\desktop\\微信截图_20250317143531.png";
-        System.out.println(ocr.OCR(photo));
+//        String text = FileReader.readPdf("D:\\desktop\\江澈.pdf");
+//        System.out.println(text);
+//
+//        text = FileReader.readRawMd("D:\\desktop\\report1\\report1\\report1 - team 5 - zh.md");
+//        System.out.println(text);
+//
+//        text = FileReader.readDocx("D:\\desktop\\简历.docx");
+//        System.out.println(text);
+//
+//        String photo = "D:\\desktop\\微信截图_20250317143531.png";
+//        System.out.println(ocr.OCR(photo));
     }
 }
